42fe523982593fdf87a04f9749ff0665b7e18fcf
inmanoldigitalservitec-pixel <inmanoldigitalservitec@gmail.com>
2026-06-28 15:08:35 -0400

    Mount WhatsApp panel label enhancer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,6 +17,7 @@ import { I18nProvider, useT } from "@/i18n";
 import { CrmAiFloatingChat } from "@/components/ai/CrmAiFloatingChat";
 import { InlineProjectTaskCreator } from "@/components/projects/InlineProjectTaskCreator";
 import { WhatsAppInlineTaskCreator } from "@/components/whatsapp/WhatsAppInlineTaskCreator";
+import { WhatsAppPanelLabelsEnhancer } from "@/components/whatsapp/WhatsAppPanelLabelsEnhancer";
 import appCss from "../styles.css?url";
 
 function NotFoundComponent() {
@@ -130,6 +131,7 @@ function AppShell() {
         </div>
         {currentPath === "/projects" ? <InlineProjectTaskCreator /> : null}
         {currentPath === "/whatsapp-web" ? <WhatsAppInlineTaskCreator /> : null}
+        {currentPath === "/whatsapp-web" ? <WhatsAppPanelLabelsEnhancer /> : null}
         <CrmAiFloatingChat />
       </SidebarProvider>
     </DemoTourProvider>
