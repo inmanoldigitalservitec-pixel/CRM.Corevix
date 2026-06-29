e3e949e5cbcd0559f7261c209a35b3f89d24a26d
inmanoldigitalservitec-pixel <inmanoldigitalservitec@gmail.com>
2026-06-29 15:32:45 -0400

    Constrain CRM shell height for embedded agent

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -122,11 +122,11 @@ function AppShell() {
   return (
     <DemoTourProvider>
       <SidebarProvider>
-        <div className="min-h-screen flex w-full">
+        <div className="flex h-screen w-full overflow-hidden">
           <AppSidebar />
-          <div className="flex-1 flex flex-col min-w-0">
+          <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
             <TopBar />
-            <main className={isAiRoute ? "flex-1 min-h-0 overflow-hidden" : "flex-1 overflow-auto"}>
+            <main className={isAiRoute ? "min-h-0 flex-1 overflow-hidden" : "min-h-0 flex-1 overflow-auto"}>
               <Outlet />
             </main>
           </div>
